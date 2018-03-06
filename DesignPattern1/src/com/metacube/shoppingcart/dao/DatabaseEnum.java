f0eb27d694694161fa5c92bde951f3b5ee58a6ee
Jaya Bapna <jaya.bapna@metacube.com>
2018-03-06 16:07:44 +0530

    Design Pattern 1

---
+++ DesignPattern1/src/com/metacube/shoppingcart/dao/DatabaseEnum.java
@@ -0,0 +1,9 @@
+package com.metacube.shoppingcart.dao;
+/**
+ * this enum contains storage type
+ * @author Jaya Bapna
+ *
+ */
+public enum DatabaseEnum {
+	IN_MEMORY , SQL;
+}
---
+++ DesignPattern1/src/com/metacube/shoppingcart/entity/ShoppingCart.java
@@ -0,0 +1,15 @@
+package com.metacube.shoppingcart.entity;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * this class sets or gets the shopping cart entities
+ * @author Jaya Bapna
+ *
+ */
+public class ShoppingCart extends BaseEntity{
+	public ShoppingCart() {
+		super();
+	}
+}
